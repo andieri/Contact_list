b1e58c26098945b47bfbc2a23cb7a33c795e2043
David Zilahi <david.zilahi@ge.com>
2016-06-13 16:05:16 +0200

    Mocking contacts to enable testing before rest api implementation is ready.

---
+++ src/main/java/com/ge/academy/contact_list/mock/Contact.java
@@ -0,0 +1,26 @@
+package com.ge.academy.contact_list.mock;
+
+/**
+ * Created by 212565332 on 6/13/2016.
+ */
+public class Contact {
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    String id = "id";
+    String firstName = "name";
+
+}
