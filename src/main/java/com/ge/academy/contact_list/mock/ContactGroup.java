ec5a5dde7e371278d59bc0c1ce51193ec57ec39d
David Zilahi <david.zilahi@ge.com>
2016-06-15 09:11:55 +0200

    Deleted the mock api, and depended the new rest api

--- src/main/java/com/ge/academy/contact_list/mock/ContactGroup.java
+++
@@ -1,7 +0,0 @@
-package com.ge.academy.contact_list.mock;
-
-/**
- * Created by 212565332 on 6/13/2016.
- */
-public class ContactGroup {
-}
--- src/main/java/com/ge/academy/contact_list/mock/ContactGroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ge.academy.contact_list.mock;
-
-/**
- * Created by 212566301 on 6/13/2016.
- */
-public class ContactGroupRepository {
-}
